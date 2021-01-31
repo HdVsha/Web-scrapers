b5996b5e96b8a80364e7509731fd3642c18e10ac
HdVsha <62552291+HdVsha@users.noreply.github.com>
2021-01-31 15:53:56 +0300

    Add beautiful code of the scraper

--- scraper1.py
+++ scraper1.py
@@ -1,19 +1,19 @@
-import requests
-import bs4  # or from bs4(BeautifulSoup4) import BeautifulSoup
-from urllib.request import urlopen as uReq
-import re
-
-URL = 'https://codeforces.com/contests'
-page = requests.get(URL)
-# or uClient = uReq(URL), uClient.read()
-soup = bs4.BeautifulSoup(page.content, 'html.parser')  # parsing the website  # or soup.find('body') --- will print all the content
-containers = soup.find_all('tr')
-info = []
-for container in containers:
-    tds = container.find_all('td')
-    for td in tds:
-        info.append(re.sub(r'\s+',' ',td.text))
-
-print(info)
+from requests import get
+from bs4 import BeautifulSoup
+# from urllib.request import urlopen as uReq
+from re import sub
 
 
+if __name__ == "__main__":
+    URL = 'https://codeforces.com/contests'
+    page = get(URL)
+    # or uClient = uReq(URL), uClient.read()
+    soup = BeautifulSoup(page.content, 'html.parser')  # parsing the website
+    containers = soup.find_all('tr')
+    info = []
+    for container in containers:
+        tds = container.find_all('td')
+        for td in tds:
+            info.append(sub(r'\s+',' ',td.text))
+
+    print(info)
